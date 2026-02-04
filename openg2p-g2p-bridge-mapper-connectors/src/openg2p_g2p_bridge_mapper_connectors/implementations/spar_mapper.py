d7c9d6b96667bda8429582155901df806467de80
Manoj Kumar <mkumar6@ch.iitr.ac.in>
2026-02-04 21:55:29 +0530

    Make resolve method asynchronous in SPARMapper class

--- openg2p-g2p-bridge-mapper-connectors/src/openg2p_g2p_bridge_mapper_connectors/implementations/spar_mapper.py
+++ openg2p-g2p-bridge-mapper-connectors/src/openg2p_g2p_bridge_mapper_connectors/implementations/spar_mapper.py
@@ -11,14 +11,14 @@ _logger = logging.getLogger("spar_mapper_impl")
 
 
 class SPARMapper(MapperInterface):
-    def resolve(self, resolve_request: ResolveRequest) -> ResolveResponse | None:
+    async def resolve(self, resolve_request: ResolveRequest) -> ResolveResponse | None:
         """
         Resolve the given request from Mapper and return the result.
         """
         _logger.info(f"Received resolve request: {resolve_request}")
         resolve_client = MapperResolveClient.get_component()
         try:
-            resolve_response = resolve_client.resolve_request(resolve_request)
+            resolve_response = await resolve_client.resolve_request(resolve_request)
             _logger.info("Resolve request completed successfully")
         except Exception as e:
             _logger.error(f"Failed to resolve the request: {e}")
